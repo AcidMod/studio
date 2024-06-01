ab3b9388edf3572ce692ccdf4030aa199c71f221
yumbee <156001240+CharcoalAfterlife@users.noreply.github.com>
2024-06-01 12:29:50 +0100

    Create green.js

---
+++ src/lib/themes/accent/green.js
@@ -0,0 +1,14 @@
+const guiColors = {
+    'glosstex': 'url(./glossCheckmate.png)',
+    'looks-secondary': 'hsla(133, 100%, 33%, 1)',
+    'looks-transparent': 'hsla(73, 100%, 73%, 0.35)',
+    'looks-light-transparent': 'hsla(115, 100%, 65%, 0.15)',
+    'looks-secondary-dark': 'hsla(62, 62%, 69%, 1)'
+};
+
+const blockColors = {};
+
+export {
+    guiColors,
+    blockColors
+};
