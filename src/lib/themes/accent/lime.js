6760760b435665782e4baf1a60185249498a608d
h3ldll <167717217+gvalue-04@users.noreply.github.com>
2024-06-02 18:24:53 -0700

    Create lime.js

---
+++ src/lib/themes/accent/lime.js
@@ -0,0 +1,28 @@
+const guiColors = {
+    'glosstex': 'url(./acidGloss.png)',
+    'motion-primary': '#3cb371',
+    'motion-primary-transparent': '#3cb371e6',
+    'motion-tertiary': '##3cb371',
+
+    'looks-secondary': '#2e8b57',
+    'looks-transparent': '#2e8b5759',
+    'looks-light-transparent': '#2e8b5726',
+    'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
+
+    'extensions-primary': 'hsla(10, 85%, 65%, 1)',
+    'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
+    'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
+    'extensions-light': 'hsla(10, 57%, 85%, 1)',
+
+    'drop-highlight': '#3cb371'
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#3cb371',
+    checkboxActiveBorder: '#cc3333'
+};
+
+export {
+    guiColors,
+    blockColors
+};
