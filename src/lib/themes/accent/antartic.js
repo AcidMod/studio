f95be921b18517eeeb5060978f84c07752335e5e
h3ldll <167717217+gvalue-04@users.noreply.github.com>
2024-06-04 06:18:53 -0700

    Create antartic.js

---
+++ src/lib/themes/accent/antartic.js
@@ -0,0 +1,28 @@
+const guiColors = {
+    'glosstex': 'url(./acidGloss.png)',
+    'motion-primary': '#00c3ff',
+    'motion-primary-transparent': '#00c3ffe6',
+    'motion-tertiary': '#00c3ff',
+
+    'looks-secondary': '#007bc7',
+    'looks-transparent': '#007bc759',
+    'looks-light-transparent': '#007bc726',
+    'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
+
+    'extensions-primary': 'hsla(194, 100%, 50%, 1)',
+    'extensions-tertiary': 'hsla(203, 100%, 39%, 1)',
+    'extensions-transparent': 'hsla(194, 100%, 50% 0.35)',
+    'extensions-light': 'hsla(10, 57%, 85%, 1)',
+
+    'drop-highlight': '#007bc7'
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#00c3ff',
+    checkboxActiveBorder: '#cc3333'
+};
+
+export {
+    guiColors,
+    blockColors
+};
